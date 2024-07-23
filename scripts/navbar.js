c05ccbc20c14e75446e4d6500940220acbe838c7
Nikki Reichert <nberrong@qx.net>
2024-07-23 12:00:15 -0400

    remove logo text from navbar at small size, clean up code in Prettier

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,14 +1,14 @@
-const primaryNav = document.querySelector(".topnav__links");
-const navButton = document.querySelector(".topnav__button");
+const primaryNav = document.querySelector('.topnav__links');
+const navButton = document.querySelector('.topnav__button');
 
-navButton.addEventListener("click", () => {
-    const isOpened = navButton.getAttribute("aria-expanded");
-   
-    if (isOpened === "false") {
-        navButton.setAttribute("aria-expanded", "true");
-        primaryNav.setAttribute("data-visible", "true");
-    } else {
-        navButton.setAttribute("aria-expanded", "false");
-        primaryNav.setAttribute("data-visible", "false");
-    }
-})
+navButton.addEventListener('click', () => {
+	const isOpened = navButton.getAttribute('aria-expanded');
+
+	if (isOpened === 'false') {
+		navButton.setAttribute('aria-expanded', 'true');
+		primaryNav.setAttribute('data-visible', 'true');
+	} else {
+		navButton.setAttribute('aria-expanded', 'false');
+		primaryNav.setAttribute('data-visible', 'false');
+	}
+});
