74df68c011fa2c2f1894cd0caf5e46a4c918c445
Nikki Reichert <nberrong@qx.net>
2024-07-25 18:01:05 -0400

    connecting to Google Sheets API and pulling in schedule data

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,30 @@
-require('dotenv').config();
-
 const { google } = require('googleapis');
+const keys = require('../keys.json');
 
-console.log(process.env.SHEETS_API_KEY);
+const client = new google.auth.JWT(keys.client_email, null, keys.private_key, [
+	'https://www.googleapis.com/auth/spreadsheets',
+]);
+
+client.authorize(function (err, tokens) {
+	if (err) {
+		console.log(err);
+		return;
+	} else {
+		console.log('Connected!');
+		getSchedSheet(client);
+	}
+});
+
+async function getSchedSheet(client) {
+	// get data from spreadsheet
+	const sheetsAPI = google.sheets({ version: 'v4', auth: client });
+	const options = {
+		spreadsheetId: '18_lJ-94FCqvVl1DiHL2Abw3TgyrD3nV9uOLrpdGVdZY',
+		range: 'Main',
+	};
+	let scheduleData = await sheetsAPI.spreadsheets.values.get(options);
+
+	scheduleData = scheduleData.data.values;
+
+	console.log(scheduleData);
+}
