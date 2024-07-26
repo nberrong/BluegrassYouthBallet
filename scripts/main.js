eda60a1c3c5cce91f50089bb33d1d839e4103ef9
Nikki Reichert <nberrong@qx.net>
2024-07-25 20:45:19 -0400

    creates JSON object from schedule array

--- scripts/main.js
+++ scripts/main.js
@@ -11,11 +11,11 @@ client.authorize(function (err, tokens) {
 		return;
 	} else {
 		console.log('Connected!');
-		getSchedSheet(client);
+		getSchedData(client);
 	}
 });
 
-async function getSchedSheet(client) {
+async function getSchedData(client) {
 	// get data from spreadsheet
 	const sheetsAPI = google.sheets({ version: 'v4', auth: client });
 	const options = {
@@ -27,4 +27,24 @@ async function getSchedSheet(client) {
 	scheduleData = scheduleData.data.values;
 
 	console.log(scheduleData);
+
+	//create JSON object from schedule array
+
+	//get keys from spreadsheet header
+	let keys = scheduleData[0];
+	//remove keys from main array
+	let scheduleArray = scheduleData.slice(1, scheduleData.length);
+
+	let formattedSchedule = [],
+		data = scheduleArray,
+		cols = keys,
+		l = cols.length;
+
+	for (let i = 0; i < data.length; i++) {
+		let d = data[i],
+			o = {};
+		for (let j = 0; j < l; j++) o[cols[j]] = d[j];
+		formattedSchedule.push(o);
+	}
+	console.log(formattedSchedule);
 }
