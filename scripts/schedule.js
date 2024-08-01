5f496176e6b64bd38b6a39072beb651c41c2978d
Nikki Reichert <nberrong@qx.net>
2024-08-01 13:45:14 -0400

    schedule card responsive, navbar alignment fix to last submenu

--- scripts/schedule.js
+++ scripts/schedule.js
@@ -65,12 +65,50 @@ function buildTable(data) {
 	for (let i = 0; i < data.length; i++) {
 		tbodyEl.innerHTML += `<tr>
 						<td data-cell="day" class="day-cell">${data[i].day.substring(0, 3)}</td> 
-						<td data-cell="time" class="time-cell">${data[i].start}—\n${data[i].end}</td>
+						<td data-cell="time" class="time-cell">${data[i].start} —\n${data[i].end}</td>
 						<td data-cell="type" class="type-cell">${data[i].type}</td>
 						<td data-cell="studio" class="studio-cell">${data[i].studio}</td>
 						<td data-cell="instructor" class="instructor-cell">${data[i].instructor}</td>
 				   </tr>
 		`;
 		tbodyEl.insertRow(0);
+	}
+	AddTableARIA();
+}
+
+// Function to add table ARIA from https://adrianroselli.com/2018/05/functions-to-add-aria-to-tables-and-lists.html
+function AddTableARIA() {
+	try {
+		var allTables = document.querySelectorAll('table');
+		for (var i = 0; i < allTables.length; i++) {
+			allTables[i].setAttribute('role', 'table');
+		}
+		var allCaptions = document.querySelectorAll('caption');
+		for (var i = 0; i < allCaptions.length; i++) {
+			allCaptions[i].setAttribute('role', 'caption');
+		}
+		var allRowGroups = document.querySelectorAll('thead, tbody, tfoot');
+		for (var i = 0; i < allRowGroups.length; i++) {
+			allRowGroups[i].setAttribute('role', 'rowgroup');
+		}
+		var allRows = document.querySelectorAll('tr');
+		for (var i = 0; i < allRows.length; i++) {
+			allRows[i].setAttribute('role', 'row');
+		}
+		var allCells = document.querySelectorAll('td');
+		for (var i = 0; i < allCells.length; i++) {
+			allCells[i].setAttribute('role', 'cell');
+		}
+		var allHeaders = document.querySelectorAll('th');
+		for (var i = 0; i < allHeaders.length; i++) {
+			allHeaders[i].setAttribute('role', 'columnheader');
+		}
+		// this accounts for scoped row headers
+		var allRowHeaders = document.querySelectorAll('th[scope=row]');
+		for (var i = 0; i < allRowHeaders.length; i++) {
+			allRowHeaders[i].setAttribute('role', 'rowheader');
+		}
+	} catch (e) {
+		console.log('AddTableARIA(): ' + e);
 	}
 }
